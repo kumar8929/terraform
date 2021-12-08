151f1ab0ee7377db40d2a6438df66ed525baea8f
kishore yb <37611142+kumar8929@users.noreply.github.com>
2021-12-08 16:41:12 +0530

    Add files via upload

---
+++ terraform_vpc_ec2_creation-main/modules/vpc/variables.tf
@@ -0,0 +1,18 @@
+variable "subnet" {
+  default = "10.0.0.0/24"
+}
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+variable "env" {
+ default = "test12"
+}
+variable "aws_access_key" {
+ default = "AKIAZVIYFOTWIYPSELNN"
+}
+variable "aws_secret_key" {
+ default = "****************************"
+}
+variable "vpc_security_group_ids" {
+ default = ""****************************""
+}
